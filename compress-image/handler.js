70d5b44fede66dfb852794162b456c4c3444a879
Abhinav Agrawal <abhinav.agarwal@sunstone.edu.in>
2024-03-02 12:14:52 +0530

    Initial Commit

---
+++ compress-image/handler.js
@@ -0,0 +1,11 @@
+const Sharp = require('sharp')
+
+
+exports.imagecompress = async(event) => {
+
+    console.log("Event:",event)
+    let sharpObj = new Sharp();
+    console.log("Sharp Obj:",event)
+    
+
+}

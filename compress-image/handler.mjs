ec0e5c6d1152bac31b029f145587e2b6a8348de1
Abhinav Agrawal <abhinav.agarwal@sunstone.edu.in>
2024-03-03 12:27:07 +0530

    File read from server and saved

--- compress-image/handler.mjs
+++ compress-image/handler.mjs
@@ -1,12 +1,66 @@
-import Sharp from 'compress-images';
+import compress_images from "compress-images";
+import AWS from "aws-sdk";
+import fs from "fs";
 
+const PATH_UNCOMPRESS_IMAGE = "original_image/";
+const PATH_COMPRESS_IMAGE = "";
 
+const saveImageToLocalFormS3Event = async (bucket, image_path) => {
+  let s3 = new AWS.S3();
+  let obj = await s3
+    .getObject({
+      Bucket: bucket,
+      Key: image_path,
+    })
+    .promise();
 
-export const imagecompress = async(event) => {
+  let data = await obj.Body;
 
-    console.log("Event:",event)
-    let sharpObj = new Sharp();
-    console.log("Sharp Obj:",event)
-    
+  try {
+    fs.writeFileSync(PATH_COMPRESS_IMAGE + "test.jpg", data);
+    console.log("File has been saved.");
+  } catch (error) {
+    console.error(error);
+  }
 
-}
+  return data;
+};
+
+export const imagecompress = async (event) => {
+  console.log("Event:", event);
+  let input_path = "image-1.jpeg";
+  let output_path = "";
+  saveImageToLocalFormS3Event("tetris-testbucket-v1", "images/image-1.jpg");
+
+  //   try {
+  //     compress_images(
+  //       input_path,
+  //       output_path,
+  //       { compress_force: false, statistic: true, autoupdate: true },
+  //       false,
+  //       { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+  //       { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+  //       { svg: { engine: "svgo", command: "--multipass" } },
+  //       {
+  //         gif: {
+  //           engine: "gifsicle",
+  //           command: ["--colors", "64", "--use-col=web"],
+  //         },
+  //       },
+  //       function (err, completed) {
+  //         if (completed === true) {
+  //           // Doing something.
+  //           console.log("Status:", completed);
+  //         } else {
+  //           console.log("Error:", err);
+  //         }
+  //       }
+  //     );
+  //   } catch (error) {
+  //     console.log("Catch Error:", error);
+  //   }
+
+  console.log("Sharp Obj:", event);
+};
+
+export default imagecompress;
---
+++ compress-image/index.js
@@ -0,0 +1,5 @@
+import handler from "./handler.mjs"
+
+console.log("Test")
+
+handler("text");

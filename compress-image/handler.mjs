3849dec3d08dcc44a965ae4d3ef88234b5ca9b67
Abhinav Agrawal <abhinav.agarwal@sunstone.edu.in>
2024-03-03 13:06:57 +0530

    Added method to compress image

--- compress-image/handler.mjs
+++ compress-image/handler.mjs
@@ -1,9 +1,10 @@
 import compress_images from "compress-images";
 import AWS from "aws-sdk";
 import fs from "fs";
+import path from "path";
 
 const PATH_UNCOMPRESS_IMAGE = "original_image/";
-const PATH_COMPRESS_IMAGE = "";
+const PATH_COMPRESS_IMAGE = "compress_image/";
 
 const saveImageToLocalFormS3Event = async (bucket, image_path) => {
   let s3 = new AWS.S3();
@@ -14,51 +15,58 @@ const saveImageToLocalFormS3Event = async (bucket, image_path) => {
     })
     .promise();
 
+  let file_name = await path.basename(image_path);
   let data = await obj.Body;
 
   try {
-    fs.writeFileSync(PATH_COMPRESS_IMAGE + "test.jpg", data);
+    fs.writeFileSync(PATH_UNCOMPRESS_IMAGE + file_name, data);
     console.log("File has been saved.");
   } catch (error) {
     console.error(error);
   }
 
-  return data;
+  return PATH_UNCOMPRESS_IMAGE + file_name;
+};
+
+const compressImageAtPath = async (image_path) => {
+  try {
+    compress_images(
+      image_path,
+      PATH_COMPRESS_IMAGE,
+      { compress_force: false, statistic: true, autoupdate: true },
+      false,
+      { jpg: { engine: "mozjpeg", command: ["-quality", "30"] } },
+      { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+      { svg: { engine: "svgo", command: "--multipass" } },
+      {
+        gif: {
+          engine: "gifsicle",
+          command: ["--colors", "64", "--use-col=web"],
+        },
+      },
+      function (err, completed) {
+        if (completed === true) {
+          // Doing something.
+          console.log("Status:", completed);
+        } else {
+          console.log("Error:", err);
+        }
+      }
+    );
+  } catch (error) {
+    console.log("Catch Error:", error);
+  }
 };
 
 export const imagecompress = async (event) => {
   console.log("Event:", event);
   let input_path = "image-1.jpeg";
   let output_path = "";
-  saveImageToLocalFormS3Event("tetris-testbucket-v1", "images/image-1.jpg");
-
-  //   try {
-  //     compress_images(
-  //       input_path,
-  //       output_path,
-  //       { compress_force: false, statistic: true, autoupdate: true },
-  //       false,
-  //       { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
-  //       { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
-  //       { svg: { engine: "svgo", command: "--multipass" } },
-  //       {
-  //         gif: {
-  //           engine: "gifsicle",
-  //           command: ["--colors", "64", "--use-col=web"],
-  //         },
-  //       },
-  //       function (err, completed) {
-  //         if (completed === true) {
-  //           // Doing something.
-  //           console.log("Status:", completed);
-  //         } else {
-  //           console.log("Error:", err);
-  //         }
-  //       }
-  //     );
-  //   } catch (error) {
-  //     console.log("Catch Error:", error);
-  //   }
+  let image_path = await saveImageToLocalFormS3Event(
+    "tetris-testbucket-v1",
+    "images/image-1.jpg"
+  );
+  await compressImageAtPath(image_path);
 
   console.log("Sharp Obj:", event);
 };
